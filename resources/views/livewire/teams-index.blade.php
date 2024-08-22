32ac9565822e2be100024f9def70054e7d67246a
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2024-08-21 20:33:33 -0500

    Update teams-index.blade.php

--- resources/views/livewire/teams-index.blade.php
+++ resources/views/livewire/teams-index.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5">
     @foreach($teams as $team)
         <a href="{{ route('team.index', $team) }}" class="h-20 flex flex-col justify-center items-center group hover:text-orange-500">
             <p class="text-4xl font-bold">{{ $team->division }}</p>
